abdc26f529e78c3251c7a92dfbaf341fe34330c6
Paul-Antoine Boutet <boutetp@gmail.com>
2019-01-21 00:01:26 +0100

    Ajout projet noeud CAN vide qui ne fait que répéter message + envoi d'un packet test toutes les 2 secondes

---
+++ RecCanTest/RecCanTest.ino
@@ -0,0 +1,34 @@
+#include <EEPROM.h>
+#include "Can.h"
+
+uint8_t boardId = 1;                                  // Identifiant CAN de la carte
+
+unsigned long time, stepTime = 2000;                  // Variables utilisées pour timer packet Test
+
+void setup(){
+
+  CAN.begin(CAN_80KBPS, MCP_16MHz);                   // Initialisation controleur CAN
+  CAN.initCANMasksAndFilters();                       // Configuration des filtres pour n'accepter que id = 0
+  CAN.setFilterId( 0, boardId );                      // Ajout aux filtre de l'id de la carte
+
+}
+
+void loop(){
+
+  if( millis() > time + stepTime ){                   // Envoi d'un packet Test à intervalle régulier
+    time += stepTime;
+
+    packet pTest;
+    pTest.id = boardId;
+    pTest.msg[0] = millis() / 0x100;
+    pTest.msg[1] = millis() % 0x100;
+    CAN.sendPacket( pTest );
+
+  }
+  
+  while( CAN.checkNewPacket() ){                      // Tant que packet CAN à lire
+    packet p = CAN.getNewPacket();                    // Récupération du packet CAN
+    CAN.sendPacket( p );                              // Renvoi du packet recu
+  }
+
+}
