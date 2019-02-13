d42011c11492a801b35b1deb0e76e1b76989cee8
Paul-Antoine Boutet <boutetp@gmail.com>
2019-02-13 19:39:45 +0100

    Ajout fonction de debug pour vérifier nombre de trames recues

--- RecCanTest/RecCanTest.ino
+++ RecCanTest/RecCanTest.ino
@@ -5,6 +5,8 @@ uint16_t boardId;                                      // Identifiant CAN de la 
 
 unsigned int pos;                                      // Simulation position du servo
 
+unsigned int cptPacket;
+
 void setup(){
 
   Serial.begin( 500000 );
@@ -17,7 +19,14 @@ void setup(){
 
 }
 
+unsigned long time, stepTime = 500;
+
 void loop(){
+
+  if( millis() > time + stepTime ){
+    time += stepTime;
+    Serial.println( cptPacket );
+  }
   
   while( CAN.checkNewPacket() ){                      // Tant que packet CAN à lire
     canPacket p = CAN.getNewPacket();                 // Récupération du packet CAN
@@ -25,24 +34,41 @@ void loop(){
     byte command = p.msg[0];
     byte servoId = p.msg[1];
 
-    Serial.println("Nouvelle Reception :");
-    Serial.print( "Command N°" );
-    Serial.print( command );
-    Serial.print( ", ServoID N°" );
-    Serial.println( servoId );
+//    Serial.println("Nouvelle Reception :");
+//    Serial.print( "Command N°" );
+//    Serial.print( command );
+//    Serial.print( ", ServoID N°" );
+//    Serial.println( servoId );
 
     switch( command ){
 
       case 1 : {                                      // PositionAsk
-          p.msg[0] = 2;                               // Retour avec la commande Position Response
+          p.msg[0] = 2;                               // => PositionResponse
           p.msg[2] = pos >> 8;
           p.msg[3] = pos & 0xFF;
-          CAN.sendPacket( p );                        // Envoi du packet de retour
+          CAN.sendPacket( p );
         break;
       }
       
       case 3 : {                                      // SetPosition
           pos = p.msg[2] * 0x100 + p.msg[3];
+          break;
+      }
+
+      case 0xF0 : {                                   // Debug
+          cptPacket++;
+          break;
+      }
+      
+      case 0xF1 : {                                   // Debug Ask
+          p.msg[0] = 0xF2;                            // => DebugResponse
+          p.msg[2] = cptPacket >> 8;
+          p.msg[3] = cptPacket & 0xFF;
+          CAN.sendPacket( p );
+          Serial.print("cpt : ");
+          Serial.print( p.msg[2], HEX );
+          Serial.print(" ");
+          Serial.println( p.msg[3], HEX );
           break;
       }
       
