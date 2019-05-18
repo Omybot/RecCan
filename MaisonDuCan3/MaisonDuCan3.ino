068b50fa1116727c0f052798c8f1ad5c92da2390
Paul-Antoine Boutet <boutetp@gmail.com>
2019-05-18 23:30:44 +0200

    Programmes sans Serial

---
+++ MaisonDuCan3/MaisonDuCan3.ino
@@ -0,0 +1,135 @@
+#include <Ethernet.h>
+#include "mcp_can.h"
+
+/////////////////////////////////
+// Gestion ETHERNET
+/////////////////////////////////
+
+#define ETH_CS_PIN  				9																	// Chip select du composant W5500
+#define ETH_MAX_PACKETSIZE 	13																	// Taille maximale d'un paquet ethernet
+
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};											// Adresse mac de la Maison du Can
+IPAddress localIp( 10, 1, 0, 15 );																// Adresse ip de la Maison du Can
+IPAddress remoteIp( 10, 1, 0, 2 );																// Adresse ip de Gobot
+unsigned int localPort = 12315;																	// port sur lequel écouter
+unsigned int remotePort = 12325;
+
+const byte udpId = 0xC5;																					// Identifiant Udp de la Maison du CAN
+const byte udpEnvoiCan = 0xC0;																			// Commande Udp utilisée pour les envoi CAN
+const byte udpReponseCan = 0xC1;																			// Commande Udp utilisée pour les retours CAN
+
+EthernetUDP Udp;																						// An EthernetUDP instance to let us send and receive packets over UDP
+
+/////////////////////////////////
+// Gestion CAN
+/////////////////////////////////
+
+#define CAN_CS_PIN  				10																	// Chip select du composant MCP2515
+#define CAN_MAX_FRAMESIZE 		8																	// Taille maximale d'une trame CAN
+
+MCP_CAN CAN( CAN_CS_PIN );																			// Instance de MCP_CAN qui permet de gérer les communications CAN
+
+/////////////////////////////////
+// SETUP
+/////////////////////////////////
+
+void setup(){
+
+	delay(200);
+
+	// Init CAN
+	while( CAN_OK != CAN.begin(CAN_500KBPS) ) delay(100);									// Initialisation communication CAN
+
+	delay(200);
+
+	// Init ETH
+	Ethernet.init( ETH_CS_PIN );																	// Réglage chip select ethernet
+	Ethernet.begin( mac, localIp );																// Initialisation connection Ethernet avec adresse MAC et IP
+	while( Udp.begin( localPort ) != 1 ) delay(100);										// Initialisation communication udp sur le port désiré
+
+	delay(200);
+
+	// Envoi trame bibon au début
+	byte udpPacketBuffer[2];
+	udpPacketBuffer[0] = udpId;
+	udpPacketBuffer[1] = 0xFF;
+	Udp.beginPacket( remoteIp, remotePort );
+	Udp.write( udpPacketBuffer, 2 );
+	Udp.endPacket();
+
+	delay(200);
+
+}
+
+/////////////////////////////////
+// LOOP
+/////////////////////////////////
+
+void loop(){
+
+	// Test reception nouvelle trame CAN
+	if( CAN_MSGAVAIL == CAN.checkReceive() ){
+
+		byte canMsgSize;
+		byte canMsg[CAN_MAX_FRAMESIZE];
+
+		// Récupération message
+		CAN.readMsgBuf( &canMsgSize, canMsg );
+		unsigned int canId = CAN.getCanId();
+
+		// Envoi sur le réseau Ethernet
+		byte udpPacketBuffer[ETH_MAX_PACKETSIZE];
+		udpPacketBuffer[0] = udpId;
+		udpPacketBuffer[1] = udpReponseCan;
+		udpPacketBuffer[2] = 0x0A;
+		udpPacketBuffer[3] = canId / 0x100;
+		udpPacketBuffer[4] = canId % 0x100;
+		for( int i=0 ; i<ETH_MAX_PACKETSIZE ; i++ ){
+			if( i<canMsgSize ) udpPacketBuffer[i+5] = canMsg[i];
+			else udpPacketBuffer[i+5] = 0x00;
+		}
+
+		Udp.beginPacket( remoteIp, remotePort );
+		Udp.write( udpPacketBuffer, ETH_MAX_PACKETSIZE );
+		Udp.endPacket();
+
+	}
+
+	// Test reception nouveau packet ETH
+	int udpPacketSize = Udp.parsePacket();
+	if( udpPacketSize ){
+
+		// remoteIp = Udp.remoteIP();																	// Mise à jour adresse IP envoyeur
+
+		// Récupération du packet ETHERNET
+		byte udpPacketBuffer[ETH_MAX_PACKETSIZE];
+		Udp.read( udpPacketBuffer, udpPacketSize );
+
+		// Test si trame à envoyer
+		if( udpPacketSize >= 5
+				&& udpPacketSize <= ETH_MAX_PACKETSIZE
+				&& udpPacketBuffer[0] == udpId
+				&& udpPacketBuffer[1] == udpEnvoiCan
+				&& udpPacketBuffer[2] == 0x0A
+		){
+
+			unsigned int canId = udpPacketBuffer[3] * 0x100 + udpPacketBuffer[4];
+			byte canMsgSize = 8;
+			byte canMsg[CAN_MAX_FRAMESIZE];
+			for( byte i=0 ; i<canMsgSize ; i++ ) canMsg[i] = udpPacketBuffer[i + 5];
+
+			// Envoi trame CAN
+			CAN.sendMsgBuf( canId, 0, canMsgSize, canMsg, 0 );
+
+		} else {
+
+			// Renvoi packet ethernet à l'envoyeur
+			Udp.beginPacket( remoteIp, remotePort );
+			Udp.write( udpPacketBuffer, udpPacketSize );
+			Udp.endPacket();
+
+		}
+
+	}
+
+}
---
+++ RecCan3/RecCan3.ino
@@ -0,0 +1,227 @@
+#include <EEPROM.h>
+#include <mcp_can.h>
+#include "MyServoHandler.h"
+#include "MyServo.h"
+
+const uint16_t boardId = 5;                                                           // Id de la carte
+
+unsigned long time;
+unsigned long stepTime = 1000;
+
+// Gestion des servos
+
+MyServoHandler servos;
+ISR(TIMER1_COMPA_vect){ servos.timer1Interrupt(); }
+ISR(TIMER2_COMPA_vect){ servos.timer2Interrupt(); }
+
+// Gestion CAN
+
+#define CAN_CS_PIN  10
+#define CAN_FRAMESIZE 8
+
+MCP_CAN CAN(CAN_CS_PIN);
+
+// Fonction qui initialise masques et filtres pour le bus CAN
+void initCANMasksAndFilters(){
+
+  // Buffer de récéption RXB0 (1 masque et 2 filtres associés)
+  CAN.init_Mask( 0, 0, 0xFFFF );          // Par défaut, le masque 1 bloque tout type d'identifiant, seul les filtres peuvent autoriser les identifiants
+  CAN.init_Filt( 0, 0, boardId );         // Filtre qui n'autorique que l'id de la carte
+  CAN.init_Filt( 1, 0, 0 );               // Filtre qui n'autorique que l'id 0
+
+  // Buffer de récéption RXB1 (1 masque et 4 filtres associés)
+  CAN.init_Mask( 1, 0, 0xFFFF );          // Par défaut, le masque 1 bloque tout type d'identifiant, seul les filtres peuvent autoriser les identifiants
+  CAN.init_Filt( 2, 0, 0 );               // Filtre qui n'autorique que l'id 0
+  CAN.init_Filt( 3, 0, 0 );               // Filtre qui n'autorique que l'id 0
+  CAN.init_Filt( 4, 0, 0 );               // Filtre qui n'autorique que l'id 0
+  CAN.init_Filt( 5, 0, 0 );               // Filtre qui n'autorique que l'id 0
+
+}
+
+bool state = false;
+
+void setup(){
+
+  servos.attach();
+
+  while( CAN_OK != CAN.begin(CAN_500KBPS) ){
+    delay(100);
+  }
+  initCANMasksAndFilters();
+
+}
+
+void loop(){
+
+  // Affichage compteur de trames
+  if( millis() > time + stepTime ){
+    time += stepTime;
+
+    state = !state;
+    digitalWrite(2, state );
+
+  }
+
+
+    if( CAN_MSGAVAIL == CAN.checkReceive() ){
+
+      // Récupération message
+      unsigned char canMsgSize;
+      unsigned char canMsg[8];
+      CAN.readMsgBuf(&canMsgSize, canMsg);
+      unsigned long canId = CAN.getCanId();
+
+      // Gestion des commandes
+      byte command = canMsg[0];
+      byte servoId = canMsg[1];
+
+      canMsgSize = 0;                             // Retour à zero pour initier message de retour
+
+      switch( command ){
+
+        case PositionAsk : {  // 0x01
+            canMsg[0] = PositionResponse;
+            canMsg[1] = servoId;
+            unsigned int position = servos.getPosition(servoId);
+            canMsg[2] = position >> 8;
+            canMsg[3] = position & 0xFF;
+            canMsgSize = 4;
+          break;
+        }
+
+        case PositionSet : {  // 0x03
+            unsigned int newPosition = canMsg[2] * 0x100 + canMsg[3];
+            servos.setPosition(servoId, newPosition);
+          break;
+        }
+
+        case PositionMinAsk : { // 0x04
+            canMsg[0] = PositionMinResponse;
+            canMsg[1] = servoId;
+            unsigned int positionMin = servos.getPositionMin(servoId);
+            canMsg[2] = positionMin >> 8;
+            canMsg[3] = positionMin & 0xFF;
+            canMsgSize = 4;
+          break;
+        }
+
+        case PositionMinSet : {  // 0x06
+            unsigned int newPositionMin = canMsg[2] * 0x100 + canMsg[3];
+            servos.setPositionMin(servoId, newPositionMin);
+          break;
+        }
+
+        case PositionMaxAsk : {  // 0x07
+            canMsg[0] = PositionMaxResponse;
+            canMsg[1] = servoId;
+            unsigned int positionMax = servos.getPositionMax(servoId);
+            canMsg[2] = positionMax >> 8;
+            canMsg[3] = positionMax & 0xFF;
+            canMsgSize = 4;
+          break;
+        }
+
+        case PositionMaxSet : {  // 0x09
+            unsigned int newPositionMax = canMsg[2] * 0x100 + canMsg[3];
+            servos.setPositionMax(servoId, newPositionMax);
+          break;
+        }
+
+        case SpeedLimitAsk : {  // 0x0A
+            canMsg[0] = SpeedLimitResponse;
+            canMsg[1] = servoId;
+            unsigned int speedLimit = servos.getSpeedLimit(servoId);
+            canMsg[2] = speedLimit >> 8;
+            canMsg[3] = speedLimit & 0xFF;
+            canMsgSize = 4;
+          break;
+        }
+
+        case SpeedLimitSet : {  // 0x0C
+            unsigned int newSpeedLimit = canMsg[2] * 0x100 + canMsg[3];
+            servos.setSpeedLimit(servoId, newSpeedLimit);
+          break;
+        }
+
+        case TorqueLimitAsk : {
+            canMsg[0] = TorqueLimitResponse;
+            canMsg[1] = servoId;
+            unsigned int torqueLimit = servos.getTorqueLimit(servoId);
+            canMsg[2] = torqueLimit >> 8;
+            canMsg[3] = torqueLimit & 0xFF;
+            canMsgSize = 4;
+          break;
+        }
+
+        case TorqueLimitSet : {  // 0x0F
+            unsigned int newTorqueLimit = canMsg[2] * 0x100 + canMsg[3];
+            servos.setTorqueLimit(servoId, newTorqueLimit);
+          break;
+        }
+
+        case TorqueAsk : {  // 0x10
+            canMsg[0] = TorqueResponse;
+            canMsg[1] = servoId;
+            unsigned int torque = servos.getTorque(servoId);
+            canMsg[2] = torque >> 8;
+            canMsg[3] = torque & 0xFF;
+            canMsgSize = 4;
+          break;
+        }
+
+        case AccelerationAsk : {  // 0x12
+            canMsg[0] = AccelerationResponse;
+            canMsg[1] = servoId;
+            unsigned int acceleration = servos.getAcceleration(servoId);
+            canMsg[2] = acceleration >> 8;
+            canMsg[3] = acceleration & 0xFF;
+            canMsgSize = 4;
+          break;
+        }
+
+        case AccelerationSet : {  // 0x14
+            unsigned int newAcceleration = canMsg[2] * 0x100 + canMsg[3];
+            servos.setAcceleration(servoId, newAcceleration);
+          break;
+        }
+
+        case TargetSet : {  // 0x15
+          break;
+        }
+
+        case TrajectorySet : {  // 0x16
+            unsigned int newPosition = canMsg[2] * 0x100 + canMsg[3];
+            unsigned int newSpeedLimit = canMsg[4] * 0x100 + canMsg[5];
+            unsigned int newAcceleration = canMsg[6] * 0x100 + canMsg[7];
+            servos.setTrajectory(servoId,newPosition, newSpeedLimit, newAcceleration);
+          break;
+        }
+
+//        case Debug : {
+//            break;
+//        }
+
+//        case DebugAsk : {
+//            canMsg[0] = DebugResponse;
+//            canMsg[1] = servoId;
+//            canMsg[2] = cptCanIn >> 8;
+//            canMsg[3] = cptCanIn & 0xFF;
+//            canMsg[4] = cptCanOut >> 8;
+//            canMsg[5] = cptCanOut & 0xFF;
+//            canMsgSize = 4;
+//            break;
+//        }
+
+      }
+
+      // Test si réponse à envoyer
+      if( canMsgSize > 0 ){
+
+        // Envoi réponse
+        CAN.sendMsgBuf( canId, 0, canMsgSize, canMsg, 0);
+
+      }
+
+    }
+
+}
