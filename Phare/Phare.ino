36e35442c55c93ad79b1979537afad02ec249b11
Paul-Antoine Boutet <boutetp@gmail.com>
2020-10-25 02:15:16 +0200

    Code a...Phare...heu

--- Phare/Phare.ino
+++ Phare/Phare.ino
@@ -4,6 +4,10 @@
 #define NUMPIXELS	12
 Adafruit_NeoPixel pixels(NUMPIXELS, PIXELPIN, NEO_GRB + NEO_KHZ800);
 
+const int switch1Pin = A1;
+const int switch2Pin = A2;
+const int motorPin = 8;
+
 unsigned long time;
 unsigned long stepTime = 150;
 
@@ -11,7 +15,35 @@ int startPixel = 0;
 
 void setup(){
 
+	// Configuration des pins
+	pinMode( switch1Pin, INPUT );
+	pinMode( switch2Pin, INPUT );
+	pinMode( motorPin, OUTPUT );
+
+	// Initialisation LEDs
 	pixels.begin();
+
+	// Detection démarrage
+	for( int j=0; j<3; j++ ){
+		while( digitalRead(switch1Pin) == LOW );
+		delay(1);
+	}
+
+	// Allumage moteur
+	digitalWrite( motorPin, HIGH );
+
+	// Detection phare en butée
+	for( int k=0; k<3; k++ ){
+		while( digitalRead(switch2Pin) == LOW );
+		delay(1);
+	}
+
+	// Extinction moteur
+	digitalWrite( motorPin, LOW );
+
+	// Démarrage séquence chenillard LED avec la LOOP
+	// ...
+
 }
 
 void loop(){
